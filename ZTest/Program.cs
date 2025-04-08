c744f0448b91cc0a74ecca721ecb624264e1daef
alej0n <alejandrex1985@gmail.com>
2025-04-08 17:50:36 +0500

    Order files fixed

--- ZTest/Program.cs
+++ ZTest/Program.cs
@@ -5,37 +5,13 @@ interface IProcesador<in T> //contravarianza generics
 {
     void Procesar(T item);
 }
-class ProcesadorAnimal : IProcesador<Animal> 
+class ProcesadorAnimal
 {
-    public void Procesar(Animal a)public class Sensor
-    {
-        public static event EventHandler TemperaturaDetectada;
-
-        public static void Detectar()
-        {
-            Console.WriteLine("Temperatura detectada (estática).");
-            TemperaturaDetectada?.Invoke(null, EventArgs.Empty);
-        }
-    }
-
-    class Programa
-    {
-        static void Main()
-        {
-            Sensor.TemperaturaDetectada += (s, e) => Console.WriteLine("⚠ Evento estático recibido");
-            Sensor.Detectar();
-        }
-    }
-
-    {
-        Console.WriteLine("Procesando un animal");
-    }
+    
 }
 class Program
 {
     static void Main()
     {
-        IProcesador<Perro> procesador = new ProcesadorAnimal(); //contravarianza IProcesador<Animal> -> IProcesador<Perro>
-        procesador.Procesar(new Perro());
     }
 }
